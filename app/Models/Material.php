e46be56de16fae9fccde726cd78d9ef5f14cf87e
gh0st <ghostofcordelia@protonmail.com>
2021-06-21 21:43:27 +0700

    refactor: Remove git merge conflict

---
+++ app/Models/Material.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Material extends Model
+{
+    use HasFactory;
+    use Uuid;
+    use LogsActivity;
+}
