c886fb9d47a45d596c0c2206057cdee064c554e2
gh0st <ghostofcordelia@protonmail.com>
2021-06-30 04:47:50 +0700

    chore: Add material dummy data factory

---
+++ database/factories/MaterialFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Material;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MaterialFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Material::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'material_type' => $this->faker->bothify('Material ##??'),
+            'material_description' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+            'volume' => $this->faker->randomElement($array = array('buah', 'kotak')),
+            'available' => $this->faker->randomDigitNotNull(),
+            'unit_price' => $this->faker->numberBetween($min = 100000, $max = 9000000),
+        ];
+    }
+}
