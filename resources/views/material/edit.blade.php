770a0ce445d11cb2219d8c0d61733ff4cea66aef
gh0st <ghostofcordelia@protonmail.com>
2021-06-30 05:04:45 +0700

    refactor: Remove unused javascript function andclean up ui

--- resources/views/material/edit.blade.php
+++ resources/views/material/edit.blade.php
@@ -11,7 +11,7 @@
     <div class="col-xl-6">
         <div id="panel-1" class="panel">
             <div class="panel-hdr">
-            <h2>Edit <span class="fw-300"><i>{{$material->nameModule->name}}</i></span></h2>
+                <h2>Edit <span class="fw-300"><i>{{$material->moduleName->name}}</i></span></h2>
                 <div class="panel-toolbar">
                     <a class="nav-link active" href="{{route('material.index')}}"><i class="fal fa-arrow-alt-left">
                         </i>
@@ -29,75 +29,83 @@
                     {!! Form::open(['route' => ['material.update',$material->uuid],'method' => 'PUT','class' =>
                     'needs-validation','novalidate']) !!}
                     <div class="form-row">
-                    <div class="form-group col-md-6 mb-3">
-                        {{ Form::label('module_category_uuid','Module Category',['class' => 'required form-label'])}}
-                        {!! Form::select('module_category_uuid', $category, $material->module_category_uuid, ['id' => 'module_category','class' => 'category
-                        form-control'.($errors->has('module_category_uuid') ? 'is-invalid':''), 'required'
-                        => '', 'placeholder' => 'Select Module Category ...']) !!}
-                        @if ($errors->has('module_category_uuid'))
-                        <div class="invalid-feedback">{{ $errors->first('module_category_uuid') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group col-md-6 mb-3">
-                        {{ Form::label('module_name','Module Name',['class' => 'required form-label'])}}
-                        <select id="module_name" class="name form-control select2" name="module_name">
-                            <option value="{{$material->nameModule->uuid}}">{{$material->nameModule->name}}
-                        </select>
-                        @if ($errors->has('module_name'))
-                        <div class="help-block text-danger">{{ $errors->first('module_name') }}</div>
-                        @endif
-                    </div>
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('module_category_uuid','Module Category',['class' => 'required form-label'])}}
+                            {!! Form::select('module_category_uuid', $category, $material->moduleName->category->uuid,
+                            ['id'
+                            => 'module_category','class' => 'category
+                            form-control'.($errors->has('module_category_uuid') ? 'is-invalid':''), 'required'
+                            => '', 'placeholder' => 'Select Module Category ...']) !!}
+                            @if ($errors->has('module_category_uuid'))
+                            <div class="invalid-feedback">{{ $errors->first('module_category_uuid') }}</div>
+                            @endif
+                        </div>
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('module_name','Module Name',['class' => 'required form-label'])}}
+                            <select id="module_name" class="name form-control select2" name="module_name">
+                                <option value="{{$material->moduleName->uuid}}">{{$material->moduleName->name}}
+                            </select>
+                            @if ($errors->has('module_name'))
+                            <div class="help-block text-danger">{{ $errors->first('module_name') }}</div>
+                            @endif
+                        </div>
                     </div>
                     <div class="form-row">
-                    <div class="form-group col-md-3 mb-3">
-                        {{ Form::label('material_type','Jenis Material',['class' => 'required form-label'])}}
-                        {{ Form::text('material_type', $material->material_type,['placeholder' => 'Deskripsi Material','class' => 'form-control '.($errors->has('material_type') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
-                        @if ($errors->has('material_type'))
-                        <div class="invalid-feedback">{{ $errors->first('material_type') }}</div>
-                        @endif
+                        <div class="form-group col-md-3 mb-3">
+                            {{ Form::label('material_type','Jenis Material',['class' => 'required form-label'])}}
+                            {{ Form::text('material_type', $material->material_type,['placeholder' => 'Jenis Material','class' => 'form-control '.($errors->has('material_type') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
+                            @if ($errors->has('material_type'))
+                            <div class="invalid-feedback">{{ $errors->first('material_type') }}</div>
+                            @endif
+                        </div>
+                        <div class="form-group col-md-3 mb-3">
+                            {{ Form::label('volume','Volume',['class' => 'required form-label'])}}
+                            {!! Form::select('volume', array('buah' => 'Buah', 'kotak' => 'Kotak'), $material->volume,
+                            ['class'
+                            => 'volume form-control'.($errors->has('volume') ?
+                            'is-invalid':''), 'required'
+                            => '']) !!}
+                            @if ($errors->has('volume'))
+                            <div class="invalid-feedback">{{ $errors->first('volume') }}</div>
+                            @endif
+                        </div>
+                        <div class="form-group col-md-2 mb-3">
+                            {{ Form::label('available','Available',['class' => 'required form-label'])}}
+                            {{ Form::text('available', $material->available,['placeholder' => 'Available','class' => 'form-control '.($errors->has('available') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
+                            @if ($errors->has('available'))
+                            <div class="invalid-feedback">{{ $errors->first('available') }}</div>
+                            @endif
+                        </div>
                     </div>
-                    <div class="form-group col-md-3 mb-3">
-                        {{ Form::label('material_description','Deskripsi Material',['class' => 'required form-label'])}}
-                        {{ Form::text('material_description', $material->material_description,['placeholder' => 'Deskripsi Material','class' => 'form-control '.($errors->has('material_description') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
-                        @if ($errors->has('material_description'))
-                        <div class="invalid-feedback">{{ $errors->first('material_description') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group col-md-3 mb-3">
-                        {{ Form::label('volume','Volume',['class' => 'required form-label'])}}
-                        {!! Form::select('volume', array('1' => 'Buah', '2' => 'Kotak'), $material->volume, ['class' => 'volume form-control'.($errors->has('volume') ?
-                        'is-invalid':''), 'required'
-                        => '', 'placeholder' => 'Select Volume ...']) !!}
-                        @if ($errors->has('volume'))
-                        <div class="invalid-feedback">{{ $errors->first('volume') }}</div>
-                        @endif
-                    </div>
-                    <div class="form-group col-md-3 mb-3">
-                        {{ Form::label('available','Available',['class' => 'required form-label'])}}
-                        {{ Form::text('available', $material->available,['placeholder' => 'Available','class' => 'form-control '.($errors->has('available') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
-                        @if ($errors->has('available'))
-                        <div class="invalid-feedback">{{ $errors->first('available') }}</div>
-                        @endif
-                    </div>
-                    </div>
-                    <div class="form-group col-md-4 mb-3">
-                        {{ Form::label('unit_price','Harga Satuan',['class' => 'required form-label'])}}
-                        <div class="input-group">
+                    <div class="class form-row">
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('material_description','Deskripsi Material',['class' => 'required form-label'])}}
+                            {{ Form::textarea('material_description', $material->material_description,['placeholder' => 'Deskripsi Material','class' => 'form-control '.($errors->has('material_description') ? 'is-invalid':''),'required', 'autocomplete' => 'off'])}}
+                            @if ($errors->has('material_description'))
+                            <div class="invalid-feedback">{{ $errors->first('material_description') }}</div>
+                            @endif
+                        </div>
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('unit_price','Harga Satuan',['class' => 'required form-label'])}}
+                            <div class="input-group">
                                 <div class="input-group-append">
                                     <span class="input-group-text">
                                         Rp.
                                     </span>
                                 </div>
-                        {{ Form::text('unit_price', $material->unit_price,['placeholder' => '','class' => 'form-control '.($errors->has('unit_price') ? 'is-invalid':''),'required', 'autocomplete' => 'off', 'data-inputmask' => "'alias': 'currency','prefix': ''"])}}
-                        @if ($errors->has('unit_price'))
-                        <div class="invalid-feedback">{{ $errors->first('unit_price') }}</div>
-                        @endif
+                                {{ Form::text('unit_price', $material->unit_price,['placeholder' => '','class' => 'form-control '.($errors->has('unit_price') ? 'is-invalid':''),'required', 'autocomplete' => 'off', 'data-inputmask' => "'alias': 'currency','prefix': ''"])}}
+                                @if ($errors->has('unit_price'))
+                                <div class="invalid-feedback">{{ $errors->first('unit_price') }}</div>
+                                @endif
+                            </div>
+                        </div>
                     </div>
-                <div
-                    class="panel-content border-faded border-left-0 border-right-0 border-bottom-0 d-flex flex-row align-items-center">
-                    <button class="btn btn-primary ml-auto" type="submit">Submit</button>
+                    <div
+                        class="panel-content border-faded border-left-0 border-right-0 border-bottom-0 d-flex flex-row align-items-center">
+                        <button class="btn btn-primary ml-auto" type="submit">Submit</button>
+                    </div>
+                    {!! Form::close() !!}
                 </div>
-                {!! Form::close() !!}
             </div>
         </div>
     </div>
@@ -129,35 +137,6 @@
                     });
                 }
             });
-        });
-        
-        // Generate a password string
-        function randString(){
-            var chars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNP123456789";
-            var string_length = 8;
-            var randomstring = '';
-            for (var i = 0; i < string_length; i++) {
-                var rnum = Math.floor(Math.random() * chars.length);
-                randomstring += chars.substring(rnum, rnum + 1);
-            }
-            return randomstring;
-        }
-        
-        // Create a new password
-        $(".getNewPass").click(function(){
-            var field = $('#password').closest('div').find('input[name="password"]');
-            field.val(randString(field));
-        });
-
-        //Enable input and button change password
-        $('#enablePassChange').click(function() {
-            if ($(this).is(':checked')) {
-                $('#passwordForm').attr('disabled',false); //enable input
-                $('#getNewPass').attr('disabled',false); //enable button
-            } else {
-                    $('#passwordForm').attr('disabled', true); //disable input
-                    $('#getNewPass').attr('disabled', true); //disable button
-            }
         });
     });
 </script>
