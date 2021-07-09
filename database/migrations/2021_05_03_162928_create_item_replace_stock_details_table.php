6eaaf5f046e80693b349ee3c15ab101267714ed9
gh0st <ghostofcordelia@protonmail.com>
2021-07-09 07:19:43 +0700

    refactor: New data structure

--- database/migrations/2021_05_03_162928_create_item_replace_stock_details_table.php
+++ database/migrations/2021_05_03_162928_create_item_replace_stock_details_table.php
@@ -17,9 +17,6 @@ class CreateItemReplaceStockDetailsTable extends Migration
             $table->id();
             $table->string('uuid')->unique();
             $table->string('item_repair_uuid')->nullable();
-            $table->string('module_category_uuid')->nullable();
-            $table->string('module_name_uuid')->nullable();
-            $table->string('module_brand_uuid')->nullable();
             $table->string('module_type_uuid')->nullable();
             $table->string('part_number')->nullable();
             $table->string('serial_number')->nullable();
