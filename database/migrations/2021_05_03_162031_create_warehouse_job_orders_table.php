aeac5207883b0857a1d33a17e7e5d4163a78efff
gh0st <ghostofcordelia@protonmail.com>
2021-07-09 07:19:10 +0700

    refactor: Remove item replace uuid column and change item status

--- database/migrations/2021_05_03_162031_create_warehouse_job_orders_table.php
+++ database/migrations/2021_05_03_162031_create_warehouse_job_orders_table.php
@@ -17,9 +17,8 @@ class CreateWarehouseJobOrdersTable extends Migration
             $table->id();
             $table->string('uuid')->unique();
             $table->string('repair_item_uuid')->nullable();
-            $table->integer('item_status')->nullable()->comment("1=Dalam penanganan oleh teknisi, 2=Telah diperbaiki oleh teknisi, 3=Tidak dapat diperbaiki oleh teknisi,4=Butuh klaim garansi, 5=Butuh penggantian barang, 6=Dalam perbaikan oleh vendor, 7=Menunggu penggantian dari vendor, 8=Telah di kirim ke customer, 9=Ticket di cancel");
+            $table->integer('item_status')->nullable()->comment("1=Dalam penanganan oleh teknisi, 2=Telah diperbaiki oleh teknisi, 3=Tidak dapat diperbaiki teknisi,4=Butuh klaim garansi, 5=Proses klaim garansi,6=Selesai Penggantian module ,7=Dalam perbaikan oleh vendor, 8=Menunggu penggantian dari vendor, 9=Telah di kirim ke customer, 10=Ticket di cancel");
             $table->integer('job_status')->nullable()->comment("0=Open, 1=Closed, 2=Cancel");
-            $table->string('item_replace_uuid')->nullable();
             $table->text('notes')->nullable();
             $table->string('resi_image')->nullable();
             $table->string('created_by')->nullable();
