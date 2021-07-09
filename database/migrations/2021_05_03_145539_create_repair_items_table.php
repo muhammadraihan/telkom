447270375ba78162edab65378d3ef91c86aa797a
gh0st <ghostofcordelia@protonmail.com>
2021-07-09 07:18:18 +0700

    refactor: Add item replace uuid column

--- database/migrations/2021_05_03_145539_create_repair_items_table.php
+++ database/migrations/2021_05_03_145539_create_repair_items_table.php
@@ -26,6 +26,7 @@ class CreateRepairItemsTable extends Migration
             $table->integer('warranty_status')->nullable()->comment("0=Non warranty, 1=Warranty");
             $table->integer('repair_status')->nullable()->comment("0=Tidak bisa diperbaiki, 1=Bisa diperbaiki");
             $table->integer('replace_status')->nullable()->comment("1=Stock, 2=Vendor");
+            $table->string('item_replace_uuid')->nullable();
             $table->string('created_by')->nullable();
             $table->string('edited_by')->nullable();
             $table->timestamps();
