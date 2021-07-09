00530b1c03ff3703831c38130d4d491a309f4680
gh0st <ghostofcordelia@protonmail.com>
2021-07-10 02:06:04 +0700

    feat: New ui for module detail

--- resources/views/repair/show.blade.php
+++ resources/views/repair/show.blade.php
@@ -3,86 +3,74 @@
         <div id="panel-1" class="panel">
             <div class="panel-hdr">
                 <h2>Tiket Nomor :<span class="fw-300"><i>{{ $repair_item->ticket->ticket_number }}</i></span></h2>
+                <a class="nav-link active" href="#"><i class="fal fa-calendar-alt">
+                    </i>
+                    <span
+                        class="nav-link-text">{{ \Carbon\Carbon::parse($repair_item->ticket->created_at)->translatedFormat('j F Y H:i:s') }}</span>
+                </a>
             </div>
             <div class="panel-container show">
                 <div class="panel-content">
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Module Category :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->ModuleType->brand->moduleName->category->name }}</i></span>
+                    <div class="form-row">
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('module_category','Categoy :',['class' => 'form-label'])}}
+                            {{ $repair_item->ModuleType->brand->moduleName->category->name }}
+                        </div>
+                        <div class="form-group col-md-6 mb-3">
+                            {{ Form::label('module_name','Name :',['class' => 'form-label'])}}
+                            {{ $repair_item->ModuleType->brand->moduleName->name }}
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Module Name :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->ModuleType->brand->moduleName->name }}</i></span>
+                    <div class="form-row">
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('module_brand','Brand :',['class' => 'form-label'])}}
+                            {{ $repair_item->ModuleType->brand->name }}
+                        </div>
+                        <div class="form-group col-md-6 mb-3">
+                            {{ Form::label('module_type','Type :',['class' => 'form-label'])}}
+                            {{ $repair_item->ModuleType->name }}
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Module Brand :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->ModuleType->brand->name }}</i></span>
+                    <div class="form-row">
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('part_number','Part Number :',['class' => 'form-label'])}}
+                            {{ $repair_item->part_number }}
+                        </div>
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('serial_number','Serial Number :',['class' => 'form-label'])}}
+                            {{ $repair_item->serial_number }}
+                        </div>
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('msc_number','MSC Number :',['class' => 'form-label'])}}
+                            {{ $repair_item->serial_number_msc }}
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Module Type :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->ModuleType->name }}</i></span>
+                    <div class="form-row">
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('accessories','Accesories :',['class' => 'form-label'])}}
+                            @isset($repair_item->accessories)
+                            <div>
+                                <ul class="">
+                                    @foreach ($repair_item->accessories as $item)
+                                    <li class="">
+                                        <span>{{$item}}</span>
+                                    </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            @endisset
+                        </div>
+                        <div class="form-group col-md-4 mb-3">
+                            {{ Form::label('warranty_status','Warranty Status :',['class' => 'form-label'])}}
+                            {!! Helper::WarrantyStatus($repair_item->warranty_status)!!}
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Part Number :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->part_number }}</i></span>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Serial Number :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->serial_number }}</i></span>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Serial Number MSC :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->serial_number_msc }}</i></span>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Warranty Status :</strong><span class="fw-400"><i>
-                                    @switch($repair_item->warranty_status)
-                                    @case(1)
-                                    Warranty
-                                    @break
-                                    @case(0)
-                                    Non Warranty
-                                    @break
-                                    @default
-                                    Status Unknown
-                                    @endswitch
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Kelengkapan :</strong><span class="fw-400"><i>
-                        </div>
-                        <div>
-                            <ul class="">
-                                @isset($repair_item->accessories)
-                                @foreach ($repair_item->accessories as $item)
-                                <li class="">
-                                    <span>{{$item}}</span>
-                                </li>
-                                @endforeach
-                                @endisset
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
-                        <div class="d-flex mb-2">
-                            <strong>Complain :</strong><span class="fw-400"><i>
-                                    {{ $repair_item->complain }}</i></span>
+                    <div class="form-row">
+                        <div class="form-group col-md-6 mb-3">
+                            {{ Form::label('complain','Complain:',['class' => 'form-label'])}}
+                            <div class="text-justify">
+                                {{$repair_item->complain}}
+                            </div>
                         </div>
                     </div>
                 </div>
