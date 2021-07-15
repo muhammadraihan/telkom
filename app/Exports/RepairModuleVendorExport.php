bf9e614d1feaf01a5196e6f6a53ea66997f6f19f
gh0st <ghostofcordelia@protonmail.com>
2021-07-15 17:51:55 +0700

    chore: Adding repair by vendor report export function

--- app/Exports/RepairModuleVendorExport.php
+++ app/Exports/RepairModuleVendorExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\RepairJobVendor;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
@@ -15,14 +16,39 @@ class RepairModuleVendorExport implements FromQuery, WithMapping, WithHeadings, 
 {
     use Exportable;
 
+    public function forYear(string $year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function forMonth(string $month)
+    {
+        $this->month = $month;
+
+        return $this;
+    }
+
     /**
      * @return \Illuminate\Database\Query\Builder
      */
     public function query()
     {
+
+        $year = $this->year;
+        $month = $this->month;
+
         DB::statement(DB::raw('set @rownum=0'));
-        $query = RepairJobVendor::selct([DB::raw('@rownum  := @rownum  + 1 AS rownum'), 'repair_item_uuid', 'vendor_name', 'repair_notes', 'created_at']);
-        return $query;
+        $statement = RepairJobVendor::select([DB::raw('@rownum  := @rownum  + 1 AS rownum'), 'repair_item_uuid', 'vendor_name', 'repair_notes', 'created_at'])
+            ->when($year, function ($query, $year) {
+                return $query->whereYear('created_at', $year);
+            })
+            ->when($month, function ($query, $month) {
+                return $query->whereYear('created_at', $month);
+            });
+
+        return $statement;
     }
 
     public function map($repair_vendor): array
@@ -39,6 +65,7 @@ class RepairModuleVendorExport implements FromQuery, WithMapping, WithHeadings, 
             isset($repair_vendor->repair->accessories) ? implode(',', $repair_vendor->repair->accessories) : '',
             $repair_vendor->repair->complain,
             $repair_vendor->vendor_name,
+            Carbon::parse($repair_vendor->created_at)->translatedFormat('j F Y'),
             $repair_vendor->repair_notes,
         ];
     }
@@ -56,8 +83,8 @@ class RepairModuleVendorExport implements FromQuery, WithMapping, WithHeadings, 
             'SERIAL NUMBER IM',
             'KELENGKAPAN',
             'KERUSAKAN',
-            'STATUS',
             'VENDOR',
+            'TANGGAL KEMBALI',
             'KETERANGAN',
         ];
     }
